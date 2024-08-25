41dda2fd59d0539d7cf3c5a670adb790ccd2e7ee
rasyidannas <rasshit.dsgn@gmail.com>
2024-08-25 16:06:42 +0700

    5/28. Extracting into Data Object in NodeJs.mp4

--- braiglist-scraper/index.js
+++ braiglist-scraper/index.js
@@ -3,7 +3,16 @@ const cheerio = require("cheerio");
 
 async function main() {
   const jobIndexPage = await axios.get("https://braigslist.vercel.app/jobs/1/");
-  console.log(jobIndexPage.data);
+  // console.log(jobIndexPage.data);
+  const $ = cheerio.load(jobIndexPage.data);
+  const jobs = $(".title-blob > a")
+    .map((index, element) => {
+      const tittle = $(element).text();
+      const url = $(element).attr("href");
+      return { tittle, url };
+    })
+    .get();
+  console.log(jobs);
 }
 
 main();
