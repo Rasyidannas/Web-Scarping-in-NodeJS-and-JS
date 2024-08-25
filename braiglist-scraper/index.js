278af915f3512f0793865de5c9365cab151dc6f1
rasyidannas <rasshit.dsgn@gmail.com>
2024-08-25 16:22:02 +0700

    5/29. Sccraping All Pages.mp4

--- braiglist-scraper/index.js
+++ braiglist-scraper/index.js
@@ -2,17 +2,23 @@ const axios = require("axios");
 const cheerio = require("cheerio");
 
 async function main() {
-  const jobIndexPage = await axios.get("https://braigslist.vercel.app/jobs/1/");
-  // console.log(jobIndexPage.data);
-  const $ = cheerio.load(jobIndexPage.data);
-  const jobs = $(".title-blob > a")
-    .map((index, element) => {
-      const tittle = $(element).text();
-      const url = $(element).attr("href");
-      return { tittle, url };
-    })
-    .get();
-  console.log(jobs);
+  const allJobs = [];
+  for (let i = 1; i < 14; i++) {
+    const jobIndexPage = await axios.get(
+      `https://braigslist.vercel.app/jobs/${i}/`
+    );
+    // console.log(jobIndexPage.data);
+    const $ = cheerio.load(jobIndexPage.data);
+    const jobs = $(".title-blob > a")
+      .map((index, element) => {
+        const tittle = $(element).text();
+        const url = $(element).attr("href");
+        return { tittle, url };
+      })
+      .get();
+    allJobs.push(...jobs);
+  }
+  console.log(allJobs.length);
 }
 
 main();
