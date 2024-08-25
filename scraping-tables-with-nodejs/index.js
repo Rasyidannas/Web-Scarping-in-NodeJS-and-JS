4d698091cb9f22fa34a6ce7852742a7d9738a7e0
rasyidannas <rasshit.dsgn@gmail.com>
2024-08-25 12:13:50 +0700

    4/18. Scrapping data in Nodejs with Cheerio/Request.mp4

---
+++ scraping-tables-with-nodejs/index.js
@@ -0,0 +1,15 @@
+const request = require("request-promise");
+const cheerio = require("cheerio")
+
+async function main () {
+  const result = await request.get(
+    "https://www.codingwithstefan.com/table-example/"
+  );
+
+  const $ = cheerio.load(result);
+  $("body > table > tbody > tr > td").each((index, element) => {
+    console.log($(element).text());
+  });
+}
+
+main();
