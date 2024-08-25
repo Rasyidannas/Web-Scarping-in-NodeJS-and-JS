0c5cc2b0e3a217b5ac88e0947af5497c93a4b855
rasyidannas <rasshit.dsgn@gmail.com>
2024-08-25 13:29:19 +0700

    4/20. Scrapping all table columns.mp4

--- scraping-tables-with-nodejs/index.js
+++ scraping-tables-with-nodejs/index.js
@@ -7,11 +7,19 @@ async function main () {
   );
 
   const $ = cheerio.load(result);
+  const scrapedRows = [];
+
   $("body > table > tbody > tr").each((index, element) => {
+    if(index === 0) return true;
+
     const tds = $(element).find("td");
     const company = $(tds[0]).text();
-    console.log(company);
+    const contact = $(tds[1]).text();
+    const country = $(tds[2]).text();
+    const scrapedRow = {company, contact, country};
+    scrapedRows.push(scrapedRow);
   });
+  console.log(scrapedRows);
 }
 
 main();
