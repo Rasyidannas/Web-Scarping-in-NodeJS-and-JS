6648848e56e9daff3f523373a28083eeb93628e1
rasyidannas <rasshit.dsgn@gmail.com>
2024-09-29 20:10:51 +0700

    6/39. Data Structure.mp4

--- craiglistwebscraper/index.js
+++ craiglistwebscraper/index.js
@@ -1,5 +1,18 @@
 const puppeteer = require("puppeteer");
 
+const scrappingResults = [
+  {
+    title:
+      "Python / Django or Flask / HTMX / Postgresql / AWS Developer wanted",
+    datePosted: new Date("2024-09-28 12:00:00"),
+    neighborhood: "alameda",
+    url: "https://sfbay.craigslist.org/eby/sof/d/alameda-python-django-or-flask-htmx/7788705484.html",
+    jobDescription:
+      "Help build software for an accounting firm. We primarily serve nonprofits, and our looking to build out our tooling to support both our business and our clients...",
+    compensation: "$50,000 - $75,000 per year DOE",
+  },
+];
+
 async function main() {
   const browser = await puppeteer.launch({
     headless: false,
