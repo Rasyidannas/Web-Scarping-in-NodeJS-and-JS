71ce67568addd1aaca47158595452d71e6fec6ba
rasyidannas <rasshit.dsgn@gmail.com>
2024-10-13 13:17:22 +0700

    6/65. Writing out our test.mp4

--- tdd-scrapper/__test__/parser.test.js
+++ tdd-scrapper/__test__/parser.test.js
@@ -1,12 +1,33 @@
 const parser = require("../parser");
 const fs = require("fs");
 let html;
+let listings;
 
 beforeAll(() => {
   html = fs.readFileSync("./test.html");
+  listings = parser.listings(html);
 });
 
-it("should give 4", () => {
-  const result = parser.add(2, 2);
-  expect(result).toBe(4);
+it("should give the correct number of listings", () => {
+  expect(listings.length).toBe(120);
+});
+
+it("should get hood from listing", () => {
+  expect(listings[0].hood).toBe("berkeley");
+});
+
+it("should get correct date from listing", () => {
+  expect(listings[0].datePosted).toEqual(new Date("Sun Oct 13 2024 11:21:50"));
+});
+
+it("should get correct url", () => {
+  expect(
+    listings[0].url(
+      "https://sfbay.craigslist.org/eby/muc/d/berkeley-excellent-piano-teacher/7787196212.html"
+    )
+  );
+});
+
+it("should get correct title", () => {
+  expect(listings[0]).toEqual("Excellent Piano Teacher--Teaching Online");
 });
--- tdd-scrapper/parser.js
+++ tdd-scrapper/parser.js
@@ -1,1 +1,3 @@
+const cheerio = require("cheerio");
+
 exports.add = (number1, number2) => number1 + number2;
